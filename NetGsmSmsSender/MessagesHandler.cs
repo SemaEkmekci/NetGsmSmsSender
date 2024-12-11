8954e632a66e9bdd9b70f03456d3ec954e907afe
SemaEkmekci <ekmekci.semanur@gmail.com>
2024-12-11 23:04:43 +0300

    Add project files.

---
+++ NetGsmSmsSender/MessagesHandler.cs
@@ -0,0 +1,56 @@
+﻿public static class MessagesHandler
+{
+    public static string HandleError(string responseContent)
+    {
+        var result = string.Empty;
+
+        switch (responseContent)
+        {
+            case "20":
+                result = "Hata: Mesaj metninde problem var veya maksimum karakter sayısı aşıldı.";
+                break;
+            case "30":
+                result = "Hata: Geçersiz kullanıcı adı, şifre veya API erişim izni yok.";
+                break;
+            case "40":
+                result = "Hata: Mesaj başlığı sistemde tanımlı değil.";
+                break;
+            case "50":
+                result = "Hata: Abone hesabıyla İYS kontrollü gönderim yapılamaz.";
+                break;
+            case "51":
+                result = "Hata: Aboneliğe tanımlı İYS Marka bilgisi bulunamadı.";
+                break;
+            case "70":
+                result = "Hata: Hatalı sorgulama. Parametrelerden biri hatalı veya eksik.";
+                break;
+            case "85":
+                result = "Hata: Mükerrer gönderim sınır aşıldı. Aynı numaraya 1 dakika içinde 20'den fazla gönderim yapılamaz.";
+                break;
+            case "00":
+                result = "Başarı: Görevinizin tarih formatında bir hata yok.";
+                break;
+            case "01":
+                result = "Başarı: Mesaj gönderim başlangıç tarihinde hata var, sistem tarihi ile değiştirilip işleme alındı.";
+                break;
+            case "02":
+                result = "Başarı: Mesaj gönderim sonlandırılma tarihinde hata var. Sistem tarihi ile değiştirilip işleme alındı.";
+                break;
+            case "347022009":
+                result = "Başarı: Gönderdiğiniz SMS başarıyla sistemimize ulaştı. Bu görev ID'si ile sorgulama yapabilirsiniz.";
+                break;
+            default:
+                if (responseContent.StartsWith("00 "))
+                {
+                    result = "Başarı: Gönderdiğiniz SMS başarıyla sistemimize ulaştı.\n 5 dakika boyunca ard arda gönderdiğiniz SMS'lerin sistemimiz tarafında çoklanarak (biriktirilerek) 1 dakika içerisinde gönderilecektir. Görev (bulkID) bilgisi: " + responseContent.Substring(3);
+                }
+                else
+                {
+                    result = "Hata: Bilinmeyen bir hata oluştu. Yanıt: " + responseContent;
+                }
+                break;
+        }
+
+        return result;
+    }
+}
---
+++ NetGsmSmsSender/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        string apiUrl = "https://api.netgsm.com.tr/sms/send/get/";
+
+        var parameters = new Dictionary<string, string>
+        {
+            { "usercode", "850XXXXXXX" },
+            { "password", "sifre" },
+            { "gsmno", "905XXXXXXXXX" },
+            { "message", "Deneme mesajıdır." },
+            { "msgheader", "BASLIK" },
+            { "startdate", "" },
+            { "stopdate", "" },
+            { "dil", "TR" },
+        };
+
+        try
+        {
+            var formData = new FormUrlEncodedContent(parameters);
+            HttpResponseMessage response = await client.PostAsync(apiUrl, formData);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Yanıt: " + responseContent.ToString() +"\n"+MessagesHandler.HandleError(responseContent));
+            }
+         
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message);
+        }
+    }
+}
